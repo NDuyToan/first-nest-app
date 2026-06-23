aea38c7859dc12a0d9c3dd6b18d3e562cb1245de
toan_nguyen <nguyenduytoan@bkdn@gmail.com>
2026-06-23 10:14:00 +0700

    tuan 2: 3-xu ly query param va pagination

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 
 import { createApiResponse } from 'src/common/decorators/api-response.decorator';
+import { PaginationQueryDto } from './dto/pagination-query.tdo';
 
 @Controller('products')
 export class ProductsController {
@@ -48,6 +49,52 @@ export class ProductsController {
       data: filteredProducts,
       filters: { category, minPrice, maxPrice },
       count: filteredProducts.length,
+    };
+  }
+
+  @Get('articles')
+  getArticles(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+  ) {
+    const pageNum = parseInt(page);
+    const limitNum = parseInt(limit);
+    const offset = (pageNum - 1) * limitNum;
+
+    // gỉa lập dữ liệu và tính toán pagination
+    const totalItems = 100; // tổng số bài viết
+    const totalPage = Math.ceil(totalItems / limitNum);
+
+    return {
+      data: [],
+      currentPage: pageNum,
+      itemsPerPage: limitNum,
+      totalItems,
+      totalPage,
+      hasNextPage: pageNum < totalPage,
+      hasPreviousPage: pageNum > 1,
+    };
+  }
+
+  @Get('products-dto')
+  getProductsWithDto(@Query() paginationDto: PaginationQueryDto) {
+    const { page, limit } = paginationDto;
+
+    const offset = (page - 1) * limit;
+
+    const total = 150;
+    const totalPage = Math.ceil(total / limit);
+
+    return {
+      data: [],
+      meta: {
+        page,
+        limit,
+        total,
+        totalPage,
+        hasNextPage: page < totalPage,
+        hasPreviousPage: page > 1,
+      },
     };
   }
 }
