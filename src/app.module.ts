d2e2f7f177469008bdf59562cf8be0a55b78b493
toan_nguyen <nguyenduytoan@bkdn@gmail.com>
2026-06-25 09:19:10 +0700

    tuan 4: 1 - cai dat type orm va cau hinh database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,38 @@
-import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { HelloController } from './hello/hello.controller';
+import { Module } from '@nestjs/common';
 import { ProductsController } from './products/products.controller';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, TasksModule],
+  imports: [
+    UsersModule,
+    TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [User],
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
+      }),
+
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController, HelloController, ProductsController],
   providers: [AppService],
 })
