834a7bc5e7ef011db52084eaa36b6a167c3e00b9
toan_nguyen <nguyenduytoan@bkdn@gmail.com>
2026-06-26 10:31:58 +0700

    remove users, posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloController } from './hello/hello.controller';
 import { Module } from '@nestjs/common';
-import { Post } from './posts/post.entity';
-import { PostsModule } from './posts/posts.module';
 import { ProductsController } from './products/products.controller';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    UsersModule,
     TasksModule,
     ConfigModule.forRoot({
       isGlobal: true,
@@ -28,13 +23,12 @@ import { UsersModule } from './users/users.module';
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
-        entities: [User, Post],
+        entities: [],
         synchronize: configService.get<boolean>('DB_SYNCHRONIZE'),
       }),
 
       inject: [ConfigService],
     }),
-    PostsModule,
   ],
   controllers: [AppController, HelloController, ProductsController],
   providers: [AppService],
